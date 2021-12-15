2bcbad70c3d955e80973691b0ca585a9c370f0c6
junjie <1652094603@qq.com>
2021-12-15 20:34:49 +0800

    0.0.3

--- demo/demo.py
+++ demo/demo.py
@@ -1,6 +1,6 @@
 from emailreminder import reminder
 
-email = '1652094603@qq.com'
+email = your_email
 
 @reminder(email=email, url='https://www.zjunjie.top/email', language='ch', name='demo1')
 def demo1(x):
